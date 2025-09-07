8b56118b98a30e4e23ecfcf326e6d7b0bcb77363
Misha Busko <Misha.1996@ukr.net>
2025-09-07 18:01:14 +0300

    Create all tasks

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,15 @@
-export const removeLastContact = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+export const removeLastContact = async () => {
+    const contacts = await readContacts();
+    if (contacts.length === 0) {
+        console.log("Контактів немає");
+        return;
+    }
+    const updatedContacts = contacts.slice(0, -1);
+    await writeContacts(updatedContacts);
+    console.log("Останній контакт видалено");
+};
 
 removeLastContact();
